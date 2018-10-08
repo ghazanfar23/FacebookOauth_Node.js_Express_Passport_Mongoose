5fd3db59753a5cd3b13826b8f6419c7acd61d321
Ghazanfar Ali <ghazanfarali23@gmail.com>
2018-10-07 23:34:45 -0500

    include and initiate passport to Facebook API and created basic route handlers

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require("express");
+
+const keys = require("./configs/keys");
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
+
 const app = express();
+
 const PORT = 55000;
 
-app.get("/", function(req, res) {
-  res.send("hello world");
-});
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.fb_appID,
+      clientSecret: keys.fb_appSecret,
+      callbackURL: "http://localhost:55000/auth/facebook/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
 
 app.listen(PORT);
